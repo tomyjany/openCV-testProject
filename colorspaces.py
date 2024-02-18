80ebb06b8d1830cc3fc9c5596c2ecadbcf29b4d6
tomyjany <tomas.janovec@tul.cz>
2024-02-18 19:13:42 +0100

    test opencv

---
+++ colorspaces.py
@@ -0,0 +1,5 @@
+import cv2 as cv
+
+
+def print_all_cv_attributes():
+    print(dir(cv))
--- main.py
+++ main.py
@@ -3,14 +3,17 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import colorspaces
+
 
 def print_hi(name):
-    # Use a breakjkkpoint in the code line below to debug your script.
+    # Use a break point in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Tohle je muj prvni projekt v Pycharmu')
+    colorspaces.print_all_cv_attributes()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
